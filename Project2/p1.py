2d91ad5819669c669292d9f16e206060e1ad4aa2
Maroof <maroofmf@gmail.com>
2016-02-12 16:42:38 -0800

    p1.py of Project2 Updated

--- Project2/p1.py
+++ Project2/p1.py
@@ -1,11 +1,57 @@
+"""
+Author: Maroof Mohammed Farooq
+USC ID: 7126869945
+EE 511
+Project 2
+Problem 1
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
+class rngWeibull():
+    def __init__(self,shapeParam,totalSamples):
+        self.totalSamples = totalSamples
+        self.shapeParam = shapeParam
+        self.randomGenerator=[]
+
+    def uniformGenerator(self):
+        return(np.random.uniform(0,1,size=1)[0])
+
+    def weibullGenerator(self):
+        for size in range(0,self.totalSamples):
+            randNumber = ((-math.log1p(-self.uniformGenerator()))**(1/self.shapeParam))
+            self.randomGenerator.append(randNumber)
+
+        return(self.randomGenerator)
 
 
-def weib(x,n,a):
-    return (a / n) * (x / n)**(a - 1) * np.exp(-(x / n)**a)
-a=5
-count, bins, ignored = plt.hist(np.random.weibull(a,1000))
-plt.show()
 
-print(bins)
+
+def main():
+    randomNumbersObject = rngWeibull(5,1000)
+    randomNumbers = randomNumbersObject.weibullGenerator()
+
+
+
+
+
+    #Plotting
+    plt.figure(1)
+    plt.hist(randomNumbers)
+    plt.figure(2)
+    plt.hist(np.random.weibull(5,1000))
+    plt.show()
+
+
+
+
+
+
+#BoilerPlate Syntax
+if __name__ == '__main__':
+    main()
+
+
+
